ca8ce565794e17d1e9afe2edc02353823fb916da
Henry Soicher <henrysoicher18@gmail.com>
2025-09-02 18:09:35 -0700

    Update KSAN.py

--- KSAN.py
+++ KSAN.py
@@ -46,11 +46,11 @@ _SESS = requests.Session(); _SESS.headers.update(BROWSER_HEADERS)
 MATRIX_ROWS, MATRIX_COLS = 32, 64
 HARDWARE_MAPPING = "adafruit-hat"
 
-PWM_BITS = 11
-PWM_LSB_NS = 100
+PWM_BITS = 10
+PWM_LSB_NS = 70
 GPIO_SLOWDOWN = 6
 BRIGHTNESS = 80
-LIMIT_REFRESH_HZ = 500
+LIMIT_REFRESH_HZ = 259
 
 FONT_SMALL_CANDIDATES = [
     "/home/henry/rpi-rgb-led-matrix/fonts/6x10.bdf",
